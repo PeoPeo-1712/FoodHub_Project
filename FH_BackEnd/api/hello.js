cce0d416f9498947a42e134bf5d22dc6f7c2889d
Bảo Nguyễn <baonguyenhuynhquocbl03@gmail.com>
2025-09-26 15:07:35 +0700

    build: set up hold environment for production

--- FH_BackEnd/api/hello.js
+++ FH_BackEnd/api/hello.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("", (req, res) => {
   res.json({ message: "Hello từ FH_BackEnd!" });
 });
 
