164e52a452689cc9a256a055390e0ceb29c55adf
Bảo Nguyễn <baonguyenhuynhquocbl03@gmail.com>
2025-09-26 14:26:34 +0700

    build: set up hold environment for production

--- FH_BackEnd/api/hello.js
+++ FH_BackEnd/api/hello.js
@@ -1,13 +1,10 @@
 import express from "express";
 import serverless from "serverless-http";
-import cors from "cors";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello từ Vercel serverless!" });
+  res.json({ message: "Hello từ serverless FH_BackEnd!" });
 });
 
 export default serverless(app);
