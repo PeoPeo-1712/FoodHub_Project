30e078f634dbdd8b6d21ced3a7e62dbcfd04bbd5
Bảo Nguyễn <baonguyenhuynhquocbl03@gmail.com>
2025-09-26 14:08:11 +0700

    build: set up hold environment for production

--- FH_BackEnd/server.js
+++ FH_BackEnd/server.js
@@ -2,18 +2,16 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend!" });
+  res.json({ message: "Hello from backend dev!" });
 });
 
-// Chỉ dev mới listen port
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`Dev server running on port ${PORT}`));
 }
 
-export default app; // Export để serverless
+export default app;
