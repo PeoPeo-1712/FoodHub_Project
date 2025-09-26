0c3956703b0b3d1e3ee527ae34a15962be835e9c
Bảo Nguyễn <baonguyenhuynhquocbl03@gmail.com>
2025-09-26 14:06:37 +0700

    build: set up hold environment for production

--- FH_FrontEnd/src/App.jsx
+++ FH_FrontEnd/src/App.jsx
@@ -4,10 +4,11 @@ export default function App() {
   const [msg, setMsg] = useState("");
 
   useEffect(() => {
-    const apiUrl = import.meta.env.VITE_API_URL || "/api"; // dev: localhost, prod: /api
+    const apiUrl = import.meta.env.VITE_API_URL || "/api";
     fetch(`${apiUrl}/hello`)
       .then(res => res.json())
       .then(data => setMsg(data.message));
   }, []);
+
   return <div>{msg}</div>;
 }
